c2cc0fca40d8d9b4cb7bb666780414d061328212
theneelshah <theneelshah@gmail.com>
2023-11-20 22:22:57 -0800

    Use resume instead of repo

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -124,12 +124,12 @@ export const banner = (args?: string[]): string => {
       #    #    #  #       #  # #  #       #       #            #  #    #  ######  #    # 
       #    #    #  #       #   ##  #       #       #       #    #  #    #  #    #  #    #
       #    #    #  ######  #    #  ######  ######  ######   ####   #    #  #    #  #    #
-</div>
+  </div>
 
 Hey there! I'm Neel Shah, currently doing my Masters of Software Engineering at University of California, Irvine.
 
 Type 'help' to see the list of available commands.
 Type 'summary' to display summary.
-Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+Type 'resume' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.resume_url}" target="_blank">here</a></u> for my resume.
 `;
 };
